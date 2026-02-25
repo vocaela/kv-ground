52abe79e9f49be5f00925578a851060522f976ac
Jianwen Zhang <jianwenzh@gmail.com>
2026-02-25 09:01:36 -0800

    Fix extra "python" in eval script

--- run_eval.sh
+++ run_eval.sh
@@ -7,7 +7,7 @@ OUTPUT_DIR="/path/to/output"
 # ScreenSpot-Pro
 # The Screenspot-Pro data downloaded from https://huggingface.co/datasets/likaixin/ScreenSpot-Pro/tree/main
 SSPRO_DATA_DIR="/path/to/screenspot-pro-data"
-torchrun --nproc_per_node=8 python eval_sspro_hf_dp.py \
+torchrun --nproc_per_node=8 eval_sspro_hf_dp.py \
   --model_dir "$MODEL_DIR" \
   --data_dir "$SSPRO_DATA_DIR" \
   --output_dir "$OUTPUT_DIR" \
@@ -16,7 +16,7 @@ torchrun --nproc_per_node=8 python eval_sspro_hf_dp.py \
 # The Screenspot-v2 data downloaded from https://huggingface.co/datasets/OS-Copilot/ScreenSpot-v2
 # Note that need first unzip screenspotv2_image.zip
 SSV2_DATA_DIR="/path/to/screenspot-v2-data"
-torchrun --nproc_per_node=8 python eval_ssv2_hf_dp.py \
+torchrun --nproc_per_node=8 eval_ssv2_hf_dp.py \
   --model_dir "$MODEL_DIR" \
   --data_dir "$SSV2_DATA_DIR" \
   --output_dir "$OUTPUT_DIR" \
@@ -24,13 +24,13 @@ torchrun --nproc_per_node=8 python eval_ssv2_hf_dp.py \
 # OSWorld-G
 # The OSWorldG data downloaded from https://github.com/xlang-ai/OSWorld-G/tree/main/benchmark
 OSWORLDG_DATA_DIR="/path/to/osworldg-data"
-torchrun --nproc_per_node=8 python eval_osworldg_hf_dp.py \
+torchrun --nproc_per_node=8 eval_osworldg_hf_dp.py \
   --model_dir "$MODEL_DIR" \
   --data_dir "$OSWORLDG_DATA_DIR" \
   --output_dir "$OUTPUT_DIR" \
 
 # OSWorld-G-refined
-torchrun --nproc_per_node=8 python eval_osworldg_hf_dp.py \
+torchrun --nproc_per_node=8 eval_osworldg_hf_dp.py \
   --model_dir "$MODEL_DIR" \
   --data_dir "$OSWORLDG_DATA_DIR" \
   --refined true \
@@ -39,7 +39,7 @@ torchrun --nproc_per_node=8 python eval_osworldg_hf_dp.py \
 # UI-vision
 # The ui-vision data downloaded from https://huggingface.co/datasets/ServiceNow/ui-vision/tree/main
 UIVISION_DATA_DIR="/path/to/uivision-data"
-torchrun --nproc_per_node=8 python eval_uivision_hf_dp.py \
+torchrun --nproc_per_node=8 eval_uivision_hf_dp.py \
   --model_dir "$MODEL_DIR" \
   --data_dir "$UIVISION_DATA_DIR" \
   --output_dir "$OUTPUT_DIR" \
